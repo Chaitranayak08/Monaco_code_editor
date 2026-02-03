d5cf0247f27ccd903ec5ecb9477dc27db0061d96
Chaitra Nayak <Chaitra@sierralearningacademy.com>
2026-02-03 23:16:01 +0530

    Fix Monaco Vite build for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import monacoEditorPlugin from 'vite-plugin-monaco-editor'
+import MonacoEditorPlugin from 'vite-plugin-monaco-editor'
 
 export default defineConfig({
   plugins: [
     react(),
-    monacoEditorPlugin({
+    MonacoEditorPlugin.default({
       languageWorkers: ['editorWorkerService', 'typescript', 'json']
     })
   ]
